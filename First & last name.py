efc23872df4b719378a10ddfae6fee92ab20baae
Pete <PHutchison@qa.com>
2021-09-03 11:35:01 +0100

    Added First & last name tutorial

---
+++ First & last name.py
@@ -0,0 +1,6 @@
+# First & last name tutorial
+
+first_name = input("Hello, what is your first name?")
+last_name = input("And what is your last name?")
+
+print("Hello " + first_name + " " + last_name)
