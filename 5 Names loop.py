a2554d57ac6a8bff649bd40312bf96a11ac5e685
Pete <PHutchison@qa.com>
2021-09-06 14:59:47 +0100

    Added 5 Names loop

---
+++ 5 Names loop.py
@@ -0,0 +1,13 @@
+print("Enter 5 names right now or else I'll come over there and make you enter them")
+name1 = input("Name 1:")
+name2 = input("Name 2:")
+name3 = input("Name 3:")
+name4 = input("Name 4:")
+name5 = input("Name 5:")
+
+name_list = [name1, name2, name3, name4, name5]
+
+count = 0
+while count < 5:
+    print(name_list[count] + " is awesome!")
+    count += 1
