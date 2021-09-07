85f17a2260c6791777f2c71a8c20602414b4fc8c
Pete <PHutchison@qa.com>
2021-09-07 15:38:44 +0100

    Added folders

---
+++ Modules/dice file.py
@@ -0,0 +1,6 @@
+import dice_function
+
+num1 = dice_function.dice()
+num2 = dice_function.dice()
+
+print("First roll was a " + str(num1) + "\nAnd the second roll was a " + str(num2))
---
+++ Modules/dice_function.py
@@ -0,0 +1,5 @@
+import random
+
+def dice():
+    dice_num = random.randint(1,6)
+    return dice_num
