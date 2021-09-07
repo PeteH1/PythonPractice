1232416ec30148980a3f6a95b04b2ab19522ff52
Pete <PHutchison@qa.com>
2021-09-07 16:50:56 +0100

    Edited grading

--- Slightly harsh grading.py
+++ Slightly harsh grading.py
@@ -17,14 +17,28 @@ def overall_grade(total_perc):
         final_grade = "FAIL"
     return final_grade
 
+# Generates a message based on a grade
+def final_message(input_grade):
+    if input_grade == "A":
+        output_message = "This is acceptable."
+    elif input_grade == "B":
+        output_message = "This is almost acceptable."
+    elif input_grade == "C":
+        output_message = "What a disappointing result."
+    else:
+        output_message = "How awful."
+    return output_message
+
 student_name = input("Enter name: ")
 hw_score = int(input("Enter homework score (out of 25): "))
 assess_score = int(input("Enter assessment score (out of 50): "))
 exam_score = int(input("Enter exam score (out of 100): "))
 
-# Calling functions to generate percentage and grade based on inputs
+# Calling functions to generate percentage,grade, and message based on inputs
 percentage = overall_percentage(hw_score, assess_score, exam_score)
 grade = overall_grade(percentage)
+message = final_message(grade)
 
 print(student_name + "'s overall percentage is " + str(percentage) + "%")
 print(student_name + "'s overall grade is " + grade)
+print(message)
