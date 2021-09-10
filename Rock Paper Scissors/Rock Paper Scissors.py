238178ead14cd47981cb85836fe5a458be8bf437
Pete <PHutchison@qa.com>
2021-09-10 15:38:09 +0100

    Updated rps

--- Rock Paper Scissors/Rock Paper Scissors.py
+++ Rock Paper Scissors/Rock Paper Scissors.py
@@ -1,15 +1,10 @@
 import random
 import time
-import pdb
 
-player_score = 0
-comp_score = 0
-options = ["Rock", "Paper", "Scissors"]
-
-def nicetype(self):
-    for letter in self:
+def nicetype(text, time_gap = 0.04):
+    for letter in text:
         print(letter, end = "")
-        time.sleep(.04)
+        time.sleep(time_gap)
      
 welcome = """WeLcOmE tO pEtE's MaGnIfIcEnT rOcK pApEr ScIsSoRs GaMe!!!!1!! <3 <3 <3
 How many games would you like to play a \"best of\"?: 
@@ -17,6 +12,10 @@ How many games would you like to play a \"best of\"?:
 nicetype(welcome)
 
 best_of = int(input(""))
+
+player_score = 0
+comp_score = 0
+options = ["Rock", "Paper", "Scissors"]
 
 while (best_of / 2) >= player_score and (best_of / 2) >= comp_score:
     player = input("Rock, Paper, or Scissors? ").capitalize()
@@ -27,9 +26,11 @@ while (best_of / 2) >= player_score and (best_of / 2) >= comp_score:
             break
         else:
             pa_message = "Nah mate you've got to spell it right otherwise it won't work :(\nPick again: "
-            nicetype(pa_message)
+            nicetype(pa_message, 0.02)
             player = input("").capitalize()
-            
+
+    countdown = "3...\n2...\n1...\n"
+    nicetype(countdown, 0.15) 
     print("You chose " + player)
     print("The computer chose " + comp)
 
@@ -61,4 +62,13 @@ while (best_of / 2) >= player_score and (best_of / 2) >= comp_score:
             player_score += 1
     print("The score is now:\nYou: " + str(player_score) + "\nComputer: " + str(comp_score) + "\n")
 
-input("Press Enter to exit")
+if player_score > (best_of / 2):
+    win_message = """CONGRATULATIONS, YOU HAVE WON!!!1!!
+Bask in your victory or press Enter to exit.
+"""
+    nicetype(win_message)
+    input("")
+else:
+    lose_message = """Unfortunately you have lost :'(
+Sit and come to terms with the misery of your defeat or press Enter to exit.
+"""
